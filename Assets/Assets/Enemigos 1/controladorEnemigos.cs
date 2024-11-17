e8a96aad611d252ac858de37ee43ad7824cb6443
iSebasX <corredorpirachican@gmail.com>
2024-11-17 12:10:03 -0500

    EnemigoControlador

---
+++ Assets/Assets/Enemigos 1/controladorEnemigos.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class controladorEnemigos : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D rg;
+    public Collider2D objetivo;
+    public float velocidad;
+    public Transform centroVision;
+    public Vector2 tamañoVision;
+    public LayerMask capaVision;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rg = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objetivo = Physics2D.OverlapBox(centroVision.position, tamañoVision, 0, capaVision);
+        anim.SetBool("jugadorDetectado", objetivo);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(centroVision.position, tamañoVision);
+    }
+}
